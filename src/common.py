3bf1b81f95d759e7f8523aba9cf74ea01a2d1104
Syed Mohammed Arshad Zaidi <arshadmail99_zaidi@yahoo.co.in>
2025-04-13 11:25:23 -0500

    Update TallRec movie data processing

--- src/common.py
+++ src/common.py
@@ -1,11 +1,44 @@
 import os
 import logging
 from pyspark.sql import SparkSession
+import shutil
+    
 
-spark = (SparkSession.builder
-         .appName('Databricks Shell')
-         .getOrCreate()
-        )
+def create_spark_session(app_name="Databricks Shell"):
+    """
+    Creates and returns a Spark session with the specified app name.
+    
+    Args:
+        app_name (str): Name for the Spark application
+        
+    Returns:
+        SparkSession: Configured Spark session
+    """
+    spark = (SparkSession.builder
+             .appName(app_name)
+             .getOrCreate()
+            )
+    
+    # Configure logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    return spark
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+
+def cleanup(data_dir):
+    """
+    Removes all files and directories within data_dir except for __init__.py.
+    
+    Args:
+        data_dir (str): Path to directory to clean
+    """
+    for item in os.listdir(data_dir):
+        item_path = os.path.join(data_dir, item)
+        if item == "__init__.py":
+            continue
+        # Remove everything else
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+        elif os.path.isdir(item_path):
+            shutil.rmtree(item_path)
